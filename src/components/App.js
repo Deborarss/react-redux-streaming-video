dc74addeba859cbed830bb2aedd7ca42bb5a1512
Debora <debora_rss@hotmail.com>
2019-05-17 14:15:31 -0300

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import '../css/App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">App</header>
+      </div>
+    );
+  }
+}
+
+export default App;
